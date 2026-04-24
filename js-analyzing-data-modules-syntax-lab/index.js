f1876f76d3ee5f3699a55f0748b8c2d9ca0ff8ee
DavidKisoi <davidkisoi51@gmail.com>
2026-04-24 05:57:25 +0300

    assignment done

--- js-analyzing-data-modules-syntax-lab/index.js
+++ js-analyzing-data-modules-syntax-lab/index.js
@@ -1,6 +1,24 @@
+require('datejs'); 
 
+function combineUsers(...args) {
+    // 1. Initialize the return object
+    let combinedObject = {
+        users: []
+    };
 
+    // 2. Loop through args and merge
+    for (let currentArray of args) {
+        combinedObject.users.push(...currentArray);
+    }
 
+    // 3. Set the date
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    // 4. Return the result
+    return combinedObject;
+}
+
+// This is the specific export the lab requires
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
